9152c9816b575fced3be67d14375cda733764527
murpyh <51100244+iimurpyh@users.noreply.github.com>
2024-10-10 15:46:12 -0400

    Removes code for claw, parallel arm, and airplane launcher

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WheelControlTestTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WheelControlTestTeleop.java
@@ -12,18 +12,14 @@ public class WheelControlTestTeleop extends LinearOpMode {
     private DcMotor rightFrontWheel = null;
     private DcMotor rightBackWheel = null;
     private DcMotor leftBackWheel = null;
-    private Servo airplaneLauncher = null;
-    private Servo claw = null;
-    private DcMotor parallelArm = null;
     private ElapsedTime runtime = new ElapsedTime();
-    private int armPosition = 0;
 
     public void runOpMode() {
         leftFrontWheel = hardwareMap.get(DcMotor.class, "leftFrontWheel");
         rightFrontWheel = hardwareMap.get(DcMotor.class, "rightFrontWheel");
         leftBackWheel = hardwareMap.get(DcMotor.class, "leftBackWheel");
         rightBackWheel = hardwareMap.get(DcMotor.class, "rightBackWheel");
-        //linearExtenderUp = hardwareMap.get(DcMotor.class, "linearExtenderUp");
+        
         leftBackWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftBackWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightBackWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -35,23 +31,10 @@ public class WheelControlTestTeleop extends LinearOpMode {
         rightBackWheel.setDirection(DcMotor.Direction.REVERSE);
         leftBackWheel.setDirection(DcMotor.Direction.REVERSE);
 
-        airplaneLauncher = hardwareMap.get(Servo.class, "airplaneLauncher");
-        claw = hardwareMap.get(Servo.class, "claw");
-        parallelArm = hardwareMap.get(DcMotor.class, "parallelArm");
-        parallelArm.setDirection(DcMotor.Direction.FORWARD);
-        parallelArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //parallelArm.setTargetPosition(0);
-        parallelArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        //parallelArm.setPower(0.01);
-        armPosition = parallelArm.getCurrentPosition();
-
-        airplaneLauncher.setPosition(0.2);
-
         waitForStart();
         runtime.reset();
 
         while (opModeIsActive()) {
-            armPosition = parallelArm.getCurrentPosition();
             double y = -gamepad1.left_stick_y;
             double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
@@ -69,36 +52,7 @@ public class WheelControlTestTeleop extends LinearOpMode {
             rightFrontWheel.setPower(frontRightPower);
             rightBackWheel.setPower(backRightPower);
 
-            // TODO: switch these to gamepad2, they'll be controlled by the second player
-            if (gamepad1.back) {
-                airplaneLauncher.setPosition(0);
-            } else {
-                airplaneLauncher.setPosition(0.7);
-            }
-
-            if (gamepad1.left_trigger >= 0.8) {
-                telemetry.addData("moving claw", true);
-                claw.setPosition(1);
-            } else {
-                claw.setPosition(0.5);
-            }
-
-            parallelArm.setPower(0);
-            if (gamepad1.a) {
-                parallelArm.setPower(-0.4);
-            }
-            if (gamepad1.x) {
-                parallelArm.setPower(0.4);
-            }
-
-
-            telemetry.addData("armPosition", armPosition);
-
             telemetry.update();
-
-            //parallelArm.setTargetPosition((int)armPositionDouble);
-
-
         }
     }
 }

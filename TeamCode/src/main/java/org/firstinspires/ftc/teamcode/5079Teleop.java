68dbc2145f7dd0f08914d96b38c2d00c86cc7a88
murpyh <51100244+iimurpyh@users.noreply.github.com>
2024-11-18 17:03:04 -0500

    WIP adds linear extender

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/5079Teleop.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="5079 Teleop", group="DriveTest")
+public class 5079Teleop extends LinearOpMode {
+    private DcMotor leftFrontWheel = null;
+    private DcMotor rightFrontWheel = null;
+    private DcMotor rightBackWheel = null;
+    private DcMotor leftBackWheel = null;
+    private DcMotor linearExtender = null;
+    private Servo clawArmControl = null;
+    private ElapsedTime runtime = new ElapsedTime();
+
+    public void runOpMode() {
+        leftFrontWheel = hardwareMap.get(DcMotor.class, "leftFrontWheel");
+        rightFrontWheel = hardwareMap.get(DcMotor.class, "rightFrontWheel");
+        leftBackWheel = hardwareMap.get(DcMotor.class, "leftBackWheel");
+        rightBackWheel = hardwareMap.get(DcMotor.class, "rightBackWheel");
+
+        linearExtender = hardwareMap.get(DcMotor.class, "linearExtender");
+        clawArmControl = hardwareMap.get(Servo.class, "clawArmControl");
+        
+        leftBackWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBackWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBackWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBackWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        
+        linearExtender.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // Set the direction motors will move
+        leftFrontWheel.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontWheel.setDirection(DcMotor.Direction.REVERSE);
+        rightBackWheel.setDirection(DcMotor.Direction.REVERSE);
+        leftBackWheel.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
+            double ry = gamepad1.right_stick_y;
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            leftFrontWheel.setPower(frontLeftPower);
+            leftBackWheel.setPower(backLeftPower);
+            rightFrontWheel.setPower(frontRightPower);
+            rightBackWheel.setPower(backRightPower);
+
+            telemetry.update();
+        }
+    }
+}

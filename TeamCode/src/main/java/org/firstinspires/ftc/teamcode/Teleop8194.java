4ce332fb9c957b6f412cf7c928c4188b61b93807
murpyh <51100244+iimurpyh@users.noreply.github.com>
2024-11-22 16:29:50 -0500

    adds 8194 templ

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop8194.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop8194.java
@@ -1,115 +1,166 @@
-//package org.firstinspires.ftc.teamcode;
-//
-//import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
-//import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-//import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
-//
-//import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-//import com.qualcomm.robotcore.hardware.CRServo;
-//import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.hardware.Servo;
-//import com.qualcomm.robotcore.util.ElapsedTime;
-//@Disabled
-//@TeleOp(name="8194 Teleop", group="DriveTest")
-//public class Teleop8194 extends LinearOpMode {
-//    private static final int LINEAR_EXTENDER_MIN = 1000;
-//    private static final int LINEAR_EXTENDER_MAX = 0;
-//    private static final int LINEAR_EXTENDER_CHANGE_RATE = 1;
-//    private static final double CLAW_ARM_MOVE_AMOUNT = -0.1;
-//
-//    private DcMotor leftFrontWheel = null;
-//    private DcMotor rightFrontWheel = null;
-//    private DcMotor rightBackWheel = null;
-//    private DcMotor leftBackWheel = null;
-//    private DcMotor linearExtender = null;
-//    private Servo clawArmControl = null;
-//    private CRServo clawSpinner = null;
-//    private ElapsedTime runtime = new ElapsedTime();
-//
-//    private int linearExtenderPosition = 0;
-//    private int linearExtenderOriginalPosition = 0;
-//    private int clawArmOriginalPosition = 0;
-//
-//    public void runOpMode() {
-//        leftFrontWheel = hardwareMap.get(DcMotor.class, "leftFrontWheel");
-//        rightFrontWheel = hardwareMap.get(DcMotor.class, "rightFrontWheel");
-//        leftBackWheel = hardwareMap.get(DcMotor.class, "leftBackWheel");
-//        rightBackWheel = hardwareMap.get(DcMotor.class, "rightBackWheel");
-//
-//        linearExtender = hardwareMap.get(DcMotor.class, "linearExtender");
-//        clawArmControl = hardwareMap.get(Servo.class, "clawArmControl");
-//        leftBackWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        leftBackWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//        rightBackWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        rightBackWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//
-//        linearExtender.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//
-//        // Set the direction motors will move
-//        leftFrontWheel.setDirection(DcMotor.Direction.FORWARD);
-//        rightFrontWheel.setDirection(DcMotor.Direction.FORWARD);
-//        rightBackWheel.setDirection(DcMotor.Direction.FORWARD);
-//        leftBackWheel.setDirection(DcMotor.Direction.FORWARD);
-//
-//        clawArmControl.setDirection(Servo.Direction.FORWARD);
-//
-//        linearExtenderPosition = linearExtenderOriginalPosition = linearExtender.getCurrentPosition();
-//
-//        waitForStart();
-//        runtime.reset();
-//
-//        while (opModeIsActive()) {
-//            double y = -gamepad1.left_stick_y;
-//            double x = gamepad1.left_stick_x;
-//            double rx = gamepad1.right_stick_x;
-//            double ry = gamepad1.right_stick_y;
-//
-//            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-//
-//            double frontLeftPower = (y + x + rx) / denominator;
-//            double backLeftPower = (y - x + rx) / denominator;
-//            double frontRightPower = (y - x - rx) / denominator;
-//            double backRightPower = (y + x - rx) / denominator;
-//
-//            leftFrontWheel.setPower(frontLeftPower);
-//            leftBackWheel.setPower(backLeftPower);
-//            rightFrontWheel.setPower(frontRightPower);
-//            rightBackWheel.setPower(backRightPower);
-//
-//            if (gamepad1.y) {
-//                clawArmControl.setPosition(clawArmOriginalPosition);
-//            } else if (gamepad1.x) {
-//                clawArmControl.setPosition(clawArmOriginalPosition + CLAW_ARM_MOVE_AMOUNT);
-//            }
-//
-//            if (gamepad1.left_bumper) {
-//                clawSpinner.setPower(1);
-//            } else {
-//                clawSpinner.setPower(0.5);
-//            }
-//
-//            // TODO: Move to gamepad2
-//            if (gamepad1.dpad_up) {
-//                linearExtenderPosition += LINEAR_EXTENDER_CHANGE_RATE;
-//            } else if (gamepad1.dpad_down) {
-//                linearExtenderPosition -= LINEAR_EXTENDER_CHANGE_RATE;
-//            }
-//
-//            if (linearExtenderPosition > (linearExtenderOriginalPosition + LINEAR_EXTENDER_MAX)) {
-//                linearExtenderPosition = (linearExtenderOriginalPosition + LINEAR_EXTENDER_MAX);
-//            }
-//            if (linearExtenderPosition < (linearExtenderOriginalPosition + LINEAR_EXTENDER_MIN)) {
-//                linearExtenderPosition = (linearExtenderOriginalPosition + LINEAR_EXTENDER_MIN);
-//            }
-//
-//            linearExtender.setTargetPosition(linearExtenderPosition);
-//
-//            telemetry.addData("clawArmControl Position:", clawArmControl.getPosition());
-//            telemetry.addData("linearExtender Position:", linearExtender.getCurrentPosition());
-//            telemetry.addData("linearExtender Target:", linearExtenderPosition);
-//            telemetry.update();
-//        }
-//    }
-//}
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@TeleOp(name = "8194 Teleop", group = "8194")
+public class Teleop8194 extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+
+    //Motor Declarations
+    private DcMotorEx leftBack = null;
+    private DcMotorEx rightBack = null;
+
+    private DcMotorEx leftFront = null;
+    private DcMotorEx rightFront = null;
+
+    private DcMotorEx bucketExtender = null;
+    private DcMotorEx intakeExtender = null;
+
+    private Servo bucketServo = null;
+    private Servo clawRotateServo = null;
+
+    private CRServo clawServo = null;
+
+    private static final int LINEAR_EXTENDER_MIN = 0;
+    private static final int LINEAR_EXTENDER_MAX = 40000;
+    private static final int LINEAR_EXTENDER_CHANGE_RATE = 30;
+
+    private int bucketExtenderPosition = 0;
+    private int bucketExtenderOriginalPosition = 0;
+
+    public void runOpMode() throws InterruptedException {
+//        try {
+            //Telemetry Initialization ################################################################
+            telemetry.setAutoClear(false);
+
+            Telemetry.Item timeVal = telemetry.addData("Elapsed Time:", runtime);
+            //------------------------------------------------------
+            Telemetry.Item SPACER1 = telemetry.addData("---------------------", "-");
+            //------------------------------------------------------
+            Telemetry.Item G1LYText = telemetry.addData("G1 LY Val:", "-");
+            Telemetry.Item G1LXText = telemetry.addData("G1 LX Val:", "-");
+            Telemetry.Item G1RXText = telemetry.addData("G1 RX Val:", "-");
+            //------------------------------------------------------
+            Telemetry.Item SPACER5 = telemetry.addData("---------------------", "-");
+            //------------------------------------------------------
+            Telemetry.Item G2LYText = telemetry.addData("G2 LY Val:", "-");
+            Telemetry.Item G2RYText = telemetry.addData("G2 RY Val:", "-");
+            Telemetry.Item G2LTText = telemetry.addData("G2 LTrigger Val:", "-");
+            Telemetry.Item G2RTText = telemetry.addData("G2 RTrigger Val:", "-");
+            Telemetry.Item BucketTargetText = telemetry.addData("Bucket Encoder Target: ", "-");
+            Telemetry.Item BucketEncoderText = telemetry.addData("Bucket Encoder Value: ", "-");
+            //##########################################################################################
+
+            //Motor Initialization #####################################################################
+            leftBack = hardwareMap.get(DcMotorEx.class, "leftBackWheel");
+            rightBack = hardwareMap.get(DcMotorEx.class, "rightBackWheel");
+            leftFront = hardwareMap.get(DcMotorEx.class, "leftFrontWheel");
+            rightFront = hardwareMap.get(DcMotorEx.class, "rightFrontWheel");
+
+            leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+            bucketExtender = hardwareMap.get(DcMotorEx.class, "bucketExtenderMotor");
+            leftBack.setDirection(DcMotorEx.Direction.REVERSE);
+            rightBack.setDirection(DcMotorEx.Direction.FORWARD);
+            rightFront.setDirection(DcMotorEx.Direction.FORWARD);
+            leftFront.setDirection(DcMotorEx.Direction.REVERSE);
+
+            bucketExtenderPosition = bucketExtenderOriginalPosition = bucketExtender.getCurrentPosition();
+            bucketExtender.setTargetPosition(bucketExtender.getCurrentPosition());
+            bucketExtender.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            bucketServo = hardwareMap.get(Servo.class, "bucketServo");
+            bucketServo.setPosition(0);
+            telemetry.addData("Status:", "Initalized");
+            waitForStart();
+            runtime.reset();
+            while (opModeIsActive()) {
+                // Velocity Vars
+                double leftBackVelocity = leftBack.getVelocity();
+                double rightBackVelocity = rightBack.getVelocity();
+                double leftFrontVelocity = leftFront.getVelocity();
+                double rightFrontVelocity = rightFront.getVelocity();
+
+                //Inputs
+                double G1LY = -gamepad1.left_stick_y;
+                double G1LX = gamepad1.left_stick_x;
+                double G1RX = gamepad1.right_stick_x;
+
+                double G2LY = -gamepad2.left_stick_y;
+                double G2RY = -gamepad2.right_stick_y;
+                double lTrigger = gamepad2.left_trigger;
+                double rTrigger = gamepad2.right_trigger;
+
+
+                //Target Setting;
+                double denominator = Math.max(Math.abs(G1LY) + Math.abs(G1LX) + Math.abs(G1RX), 1);
+                double frontLeftPower = (G1LY + G1LX + G1RX) / denominator;
+                double backLeftPower = (G1LY - G1LX + G1RX) / denominator;
+                double frontRightPower = (G1LY - G1LX - G1RX) / denominator;
+                double backRightPower = (G1LY + G1LX - G1RX) / denominator;
+
+                //Power Setting
+                leftBack.setPower(backLeftPower);
+                rightBack.setPower(backRightPower);
+                leftFront.setPower(frontLeftPower);
+                rightFront.setPower(frontRightPower);
+
+                if(gamepad1.x){
+                    bucketServo.setPosition(0);
+                }
+                else if (gamepad1.y){
+                    bucketServo.setPosition(1);
+                }
+
+                if (gamepad1.dpad_up) {
+                    bucketExtenderPosition += LINEAR_EXTENDER_CHANGE_RATE;
+                } else if (gamepad1.dpad_down) {
+                    bucketExtenderPosition -= LINEAR_EXTENDER_CHANGE_RATE;
+                }
+
+                if (bucketExtenderPosition > (bucketExtenderOriginalPosition + LINEAR_EXTENDER_MAX)) {
+                    bucketExtenderPosition = (bucketExtenderOriginalPosition + LINEAR_EXTENDER_MAX);
+                }
+                if (bucketExtenderPosition < (bucketExtenderOriginalPosition + LINEAR_EXTENDER_MIN)) {
+                    bucketExtenderPosition = (bucketExtenderOriginalPosition + LINEAR_EXTENDER_MIN);
+                }
+
+                bucketExtender.setPower(1);
+                bucketExtender.setTargetPosition(bucketExtenderPosition);
+
+                //Telemetry Updates
+                timeVal.setValue(getRuntime());
+                G1LYText.setValue(G1LY);
+                G1LXText.setValue(G1LX);
+                G1RXText.setValue(G1RX);
+
+                G2LYText.setValue(G2LY);
+                G2RYText.setValue(G2RY);
+                G2RTText.setValue(rTrigger);
+                G2LTText.setValue(lTrigger);
+                BucketTargetText.setValue(bucketExtenderPosition);
+                BucketEncoderText.setValue(bucketExtender.getCurrentPosition());
+                telemetry.update();
+
+            }
+//        } catch (Exception ignored) {
+
+
+    }
+}
